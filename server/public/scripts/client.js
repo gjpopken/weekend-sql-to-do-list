ba5172c9c17dde67fca3358e9e22cb850cada691
Gavin Popken <gavinpopkenart@gmail.com>
2023-12-15 16:32:34 -0600

    worked on get method client side, havent tested yet

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,40 @@
-console.log('JS is sourced!');
+function onStart() {
+    console.log('client is working');
+    render()
+}
+
+// let sampleTasks = [
+//     {
+//         text: 'something',
+//         isComplete: false
+//     },
+//     {
+//         text: 'something else',
+//         isComplete: true
+//     }
+// ]
+
+
+function render() {
+    axios({
+        method: 'GET',
+        url: '/todos'
+    }).then((response) => {
+        let tasksList = response.data
+        const container = document.getElementById('container')
+        for (task of tasksList) {
+            let nInnerHTML = `
+            <tr>
+                <td>${task.text} <button>Delete</button>
+            `
+            if (task.isComplete === false) {
+                nInnerHTML += `<button>Done!</button></td></tr>`
+            } else {
+                nInnerHTML += `</td></tr>`
+            }
+        }
+        container.innerHTML = nInnerHTML
+    })
+}
+
+onStart()
